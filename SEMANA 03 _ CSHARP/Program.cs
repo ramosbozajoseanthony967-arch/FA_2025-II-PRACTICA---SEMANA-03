f2c233426bea1101ee979f286b38541df4dc220f
Antony <ramosbozajoseanthony967@gmail.com>
2025-09-02 15:33:12 -0500

    subiendo archivo semana 03

---
+++ SEMANA 03 _ CSHARP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_03___CSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio1();
+            Console.ReadKey();
+        }
+        static void ejercicio1()
+        {
+            string nombre, carrera; //declarando variables
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+
+
+        }
+
+        static void ejercicio2()
+        {
+
+        }
+
+        static void ejercicio3()
+        {
+
+        }
+
+        static void ejercicio4()
+        {
+
+        }
+        static void ejercicio5()
+
+        {
+
+        }
+        static void ejercicio6()
+        {
+
+        }
+    }
+}
---
+++ SEMANA 03_PYTHON/SEMANA_03_PYTHON.py
@@ -0,0 +1,9 @@
+def ejercicio1(): #creando ejercicio 1
+    nombre = input ("Ingrese su nombre: ")
+    carrera = input ("Ingrese su carrera: ")
+
+    print (f"\n{nombre}, bienvenido a FA de {carrera}")
+
+ejer01()
+
+
